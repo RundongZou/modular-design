2e4de4167f11bd36a16ae68fce972105e44a9115
RundongZou <1809638500@qq.com>
2016-12-01 23:37:12 +0800

    2016-12-01 23:35 add 'mask'

--- service/public/components/modal/README.md
+++ service/public/components/modal/README.md
@@ -1,9 +1,9 @@
-# 按钮组件
+# modal组件
 
 ## 组件描述
 
 ##组件应用
-1. 引用按钮组件库
+1. 引用蒙层(model)组件库
 2. 被使用的元素加上class = zou-button
 
 ##应用举例
@@ -20,19 +20,3 @@
 .zou-button           一般样式
 .zou-button-warn      警告样式
 .zou-button-cancel    取消样式
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- service/public/page/js/test.js
+++ service/public/page/js/test.js
@@ -7,4 +7,19 @@
 
 define(function(require, exports, module){
     require('button');
+    require('jquery');
+    var comModal = require('modal');
+
+    $( function () {
+        var modal = comModal.init( {
+            elem : $('#con')
+        } );
+        $('.zou-button-warn').click(function () {
+            modal.show();
+            //$("#con").show();
+        });
+        $('#close-btn').click( function () {
+            modal.hide();
+        } );
+    } );
 });
