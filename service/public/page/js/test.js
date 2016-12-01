ad409acd89e3b5578ce57d2972a9452d1a3e22b0
RundongZou <1809638500@qq.com>
2016-12-01 12:59:13 +0800

    2016-12-01 12:58 my first try of modular design

---
+++ service/public/page/js/test.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Marte
+* @Date:   2016-12-01 12:29:52
+* @Last Modified by:   Marte
+* @Last Modified time: 2016-12-01 12:36:55
+*/
+
+define(function(require, exports, module){
+    require('button');
+});
